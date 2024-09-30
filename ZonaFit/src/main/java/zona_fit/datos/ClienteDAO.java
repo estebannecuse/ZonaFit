e977788e680a918af817174667967384e439e04a
Esteban Necuse <estebannecuse@gmail.com>
2024-09-30 13:34:12 -0300

    conexion a DB + eliminar cliente

--- ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
+++ ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
@@ -123,6 +123,24 @@ public class ClienteDAO implements IclienteDAO{
 
     @Override
     public boolean eliminarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = Conexion.getConnection();
+        String sql = "DELETE FROM cliente WHERE id=?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            ps.execute();
+            return true;
+        }catch(Exception e){
+            System.out.println("error al eliminar cliente" + e.getMessage());
+        }finally{
+            try{
+                con.close();
+            }catch(Exception e){
+                System.out.println("No se pudo cerrar la conexion" + e.getMessage());
+            }
+        }
+
         return false;
     }
 
@@ -163,13 +181,20 @@ public class ClienteDAO implements IclienteDAO{
         }else{
             System.out.println("No se modifico");
         }
+
+
+        // Eliminar cliente
+        var clienteEliminar = new Cliente(2);
+        var eliminado = clienteDao.eliminarCliente(clienteEliminar);
+        if(eliminado)
+            System.out.println("Cliente eliminado" + clienteEliminar);
+        else
+            System.out.println("No se pudo eliminar el cliente " + clienteEliminar);
+
         //Listop de nuevo
         System.out.println("** Listar Clientes ***");
         var clientes = clienteDao.listarClientes();
         clientes.forEach(System.out :: println);
-
-
-
 
     }
 }
---
+++ ZonaFit/src/main/java/zona_fit/presentacion/ZonaFitAPp.java
@@ -0,0 +1,7 @@
+package zona_fit.presentacion;
+
+public class ZonaFitAPp {
+    public static void main(String[] args) {
+        
+    }
+}
